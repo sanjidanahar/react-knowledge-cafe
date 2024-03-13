e5c0f448055bd99571743a3a7d39204c8fac5031
sanjidanahar <cse02707453@gmail.com>
2024-03-14 01:44:03 +0600

    blog map added

--- src/components/Blog/blog.jsx
+++ src/components/Blog/blog.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover} = blog;
     return (
         <div>
-
+            <img src={cover} alt={`cover picture of the title`} />
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
